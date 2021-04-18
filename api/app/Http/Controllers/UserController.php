4a4172045cdb887b4e9db859face36ae286b3443
nfach98 <nfach98@gmail.com>
2021-04-18 19:38:37 +0700

    fix directory

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\models\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Validator;
 
